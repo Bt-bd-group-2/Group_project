53b665954663ca4006aa830633f88552fd703397
Mustapha <moraslov@gmail.com>
2026-06-21 13:06:55 +0100

    Mustapha: i have added routes that will serve the index file and also i have middeware that log the request

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ app.use((req, res, next) => {
   next();
 });
 
-// In-memory student array. Replace with DB later
+// +In-memory student array. Replace with DB later
 let students = [
   { id: 1, name: "Aisha Bello", age: 20, course: "Computer Science" },
   { id: 2, name: "Tunde Ade", age: 22, course: "Mass Comm" },
-  
   { id: 3, name: "Doyin Adetunji", age: 27, course: "Cybersecurity" },
   { id: 4, name: "Odeyinka Tobiloba", age: 27, course: "Computer Science"},
   { id: 5, name: "Aminieli", age: 22, course: "Cyber Security" },
   { id: 6, name: "Oyinseye Okikiolaoluwa", age: 17, course: "Computer Science" }
 ];
+
 // 1. GET all students
 app.get('/students', (req, res) => {
   res.json(students);
