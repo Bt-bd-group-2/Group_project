8975eac0a7725a11a5d7da62020f98c2cfb0ca77
usmansalifu34-pixel <usmansalifu34@gmail.com>
2026-06-23 15:06:09 +0100

    added usman salifustudent data and fixed duplicate ID's

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ let students = [
   { id: 4, name: "Odeyinka Tobiloba", age: 27, course: "Computer Science"},
   { id: 5, name: "Aminieli", age: 22, course: "Cyber Security" },
   { id: 6, name: "Oyinseye Okikiolaoluwa", age: 17, course: "Computer Science" },
-  { id: 7, name: "Loise Wanjiru", age: 19, course: "Computer Science"}
-  { id: 6, name: "Lovelyn Kalu", age: 24, course: "Software Engineering" },
-  { id: 7, name: "Chiamaka Eze", age: 21, course: "Mass Communication" },
-  { id: 8, name: "Oyinseye Okikiolaoluwa", age: 17, course: "Computer Science" },
-  { id: 9, name: "Omoloye Hassan", age: 20, course: "Computer Science" },
-  { id: 10, name: "Aries Tobells", age:94, course: "Yoruba Technology" }
-  { id: 11, name: "Delphine Uwineza", age:22, course: "Software Engineering" }
+  { id: 7, name: "Loise Wanjiru", age: 19, course: "Computer Science"},
+  { id: 8, name: "Lovelyn Kalu", age: 24, course: "Software Engineering" },
+  { id: 9, name: "Chiamaka Eze", age: 21, course: "Mass Communication" },
+  { id: 10, name: "Omoloye Hassan", age: 20, course: "Computer Science" },
+  { id: 11, name: "Aries Tobells", age:94, course: "Yoruba Technology" },
+  { id: 12, name: "Delphine Uwineza", age:22, course: "Software Engineering" },
+  { id:13, name: "Usman salifu", age:17, course: "Computer Science"},
 ];
 
 // 1. GET all students
